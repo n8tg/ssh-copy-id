679d8040a7288d6ae1b72b7df1e1aa6670330133
n8 <nate@nategramer.com>
2020-11-16 20:28:22 -0600

    Added documentation, github repo, additional features

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # ssh-copy-id
-A lame PowerShell implementation of ssh-copy-id
+This is a a lame PowerShell implementation of OpenSSH's ssh-copy-id
+
+ssh-copy-id is a PowerShell script that uses ssh to log into a remote machine and append the
+indicated identity file to that machine's ~/.ssh/authorized_keys file. By default, it installs the key(s) stored in "$env:USERPROFILE\.ssh\id_rsa.pub".  
+
+CAUTION: This script is not declarative, it will append key(s) into authorized_keys that already exist. It may also be broken and overwrite your authorized_keys file. 
+
+
+## Examples
+
+### Unix username style
+    ssh-copy-id root@172.16.1.10  
+
+### Unix username style with a specified key file
+    ssh-copy-id root@172.16.1.10 -l C:\users\n8tg\SpecialKeyDir\key.pub
+
+### PowerShell parameter style with a username
+    ssh-copy-id -RemoteHost 172.16.1.10 -RemoteUser root  
+
+### PowerShell parameter style with a username and a specific key
+    ssh-copy-id -RemoteHost 172.16.1.10 -RemoteUser root -KeyFile C:\users\n8tg\SpecialKeyDir\key.pub
+
+### You can mix and match if you choose
+    ssh-copy-id -RemoteHost root@172.16.1.10 -l c:\test\test
+
---
+++ SSH-Copy-ID.psm1
@@ -0,0 +1,94 @@
+function ssh-copy-id
+{
+<#
+.SYNOPSIS
+    Appends a public key to a machines ~/.ssh/authorized_keys file.
+    CAUTION: This script is not declarative, it will append key(s) into authorized_keys that already exist.
+
+.DESCRIPTION
+    ssh-copy-id is a PowerShell script that uses ssh to log into a remote machine and append the
+    indicated identity file to that machine's ~/.ssh/authorized_keys file. By default, it installs the key(s) stored in "$env:USERPROFILE\.ssh\id_rsa.pub".
+    CAUTION: This script is not declarative, it will append key(s) into authorized_keys that already exist. 
+
+.PARAMETER RemoteHost
+    Specifies the IP or DNS name of the machine to install the public key on.
+
+.PARAMETER RemoteUser
+    Specifies which user's authorized_keys file that the key will be installed under.
+    
+.PARAMETER KeyFile
+    A path of the keyfile to be installed.
+
+.INPUTS
+
+    None at the moment.
+
+.OUTPUTS
+
+    None at the moment.
+
+.EXAMPLE
+
+    PS> ssh-copy-id root@172.16.1.10
+
+.EXAMPLE
+
+    PS> ssh-copy-id root@172.16.1.10 -l C:\users\n8tg\SpecialKeyDir\key.pub
+
+.EXAMPLE
+
+    PS> ssh-copy-id -RemoteHost 172.16.1.10 -RemoteUser root
+
+.EXAMPLE
+
+    PS> ssh-copy-id -RemoteHost 172.16.1.10 -RemoteUser root -KeyFile C:\users\n8tg\SpecialKeyDir\key.pub
+
+.NOTES
+
+    If no username is supplied using -RemoteUser or the User@RemoteHost syntax, the user running the command's username will be used.  
+
+.LINK
+
+https://github.com/n8tg/ssh-copy-id
+#>
+
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true,
+        ValueFromPipeline=$false)]
+        [string]
+        $RemoteHost,
+
+        [string]
+        $RemoteUser,
+
+        [Alias('l')]
+        [string]
+        $KeyFile = "$env:USERPROFILE\.ssh\id_rsa.pub"
+    )  
+
+    PROCESS {
+
+        if($RemoteHost -contains "@"){
+            $RemoteUser = $RemoteHost.Split("@")[0]
+            $RemoteHost = $RemoteHost.Split("@")[1]
+        }
+
+        # Check key file is there
+        if(!(Test-Path $KeyFile)) { Write-Warning "Specified key file not found"; return }
+        
+        try{
+            if($RemoteUser){
+                Get-Content $KeyFile | ssh $RemoteHost -l $RemoteUser "cd; umask 077; mkdir -p `".ssh/`" && { [ -z \`tail -1c .ssh/authorized_keys 2>/dev/null\` ] || echo >> .ssh/authorized_keys; } && cat >> .ssh/authorized_keys || exit 1; "
+            }else{
+                Get-Content $KeyFile | ssh $RemoteHost "cd; umask 077; mkdir -p `".ssh/`" && { [ -z \`tail -1c .ssh/authorized_keys 2>/dev/null\` ] || echo >> .ssh/authorized_keys; } && cat >> .ssh/authorized_keys || exit 1; "
+            }
+        } catch {
+            Write-Warning "An error occurred when installing the key"
+            Write-Host $_
+        }
+    }
+}
+
+Export-ModuleMember -Function ssh-copy-id
